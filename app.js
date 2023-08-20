04920538a3579ffbc48becaf6bccdae49941745a
ShauryaTEBSS <141617574+ShauryaTEBSS@users.noreply.github.com>
2023-08-20 21:02:45 +0530

    Update of app.js by GitBot

--- app.js
+++ app.js
@@ -35,7 +35,51 @@ useEffect(() => {
   // Stop listening for changes in state
   return function cleanup() {
     for (const event of events) {
+      // In App.js
+
+/**
+ * Creates a new call room.
+ */
+const createCall = useCallback(() => {
+  // update component state to a "creating" state...
+  return api
+    .createRoom()
+    .then(room => room.url)
+    .catch(error => {
+      // update component state to an "error" state...
+    });
+}, []);
+      // In App.js
+
+/**
+ * Starts joining an existing call.
+ */
+const startJoiningCall = useCallback(url => {
+  const callObject = DailyIframe.createCallObject();
+  // update component state to a "joining" state...
+  callObject.join({ url });
+}, []);
+      // In App.js
+
+/**
+ * Starts leaving the current call.
+ */
+const startLeavingCall = useCallback(() => {
+  if (!callObject) return;
+  // update component state to a "leaving" state...
+  callObject.leave();
+}, [callObject]);
+      
       callObject.off(event, handleNewMeetingState);
     }
   };
 }, [callObject]);
+// In App.js
+
+// The STATE_* consts below are defined in the demo app, and reflect Meeting State
+
+// Only enable "leave" button when we know we're not mid-operation
+const enableCallButtons = [STATE_JOINED, STATE_ERROR].includes(appState);
+
+// Only enable "start" button when we know we're not mid-operation
+const enableStartButton = appState === STATE_IDLE;
