0eaf9c14de6b809c11a4aba54d7f2225b397c2be
ShauryaTEBSS <141617574+ShauryaTEBSS@users.noreply.github.com>
2023-08-20 21:09:23 +0530

    Create callState.js

---
+++ callState.js
@@ -0,0 +1,18 @@
+// In callState.js
+
+function getCallItems(participants) {
+  let callItems = { ...initialCallState.callItems }; // Ensure we *always* have a local participant
+  for (const [id, participant] of Object.entries(participants)) {
+    callItems[id] = {
+      videoTrackState: participant.tracks.video,
+      audioTrackState: participant.tracks.audio,
+    };
+    if (shouldIncludeScreenCallItem(participant)) {
+      callItems[id + '-screen'] = {
+        videoTrackState: participant.tracks.screenVideo,
+        audioTrackState: participant.tracks.screenAudio,
+      };
+    }
+  }
+  return callItems;
+}
